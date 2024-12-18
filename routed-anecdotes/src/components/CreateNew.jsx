422621f1c0f0dafe4d742c62b7cbb00cfca000fb
Kris Albert Kai Lee <kai@hellokai.dev>
2024-12-18 13:59:58 +0100

    7.4-7.6

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -1,22 +1,30 @@
-import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useField } from '../hooks';
+import PropTypes from 'prop-types';
 
 const CreateNew = ({ addNew, setNotification }) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const { reset: contentReset, ...content } = useField('text');
+  const { reset: authorReset, ...author } = useField('text');
+  const { reset: infoReset, ...info } = useField('text');
 
   const navigate = useNavigate();
   const handleSubmit = e => {
     e.preventDefault();
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
-    setNotification(`a new anecdote '${content}' created!`);
+    setNotification(`a new anecdote '${content.value}' created!`);
     navigate('/');
+  };
+
+  const handleReset = e => {
+    e.preventDefault();
+    contentReset();
+    authorReset();
+    infoReset();
   };
 
   return (
@@ -25,32 +33,26 @@ const CreateNew = ({ addNew, setNotification }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={e => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={e => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={e => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   );
 };
 
 export default CreateNew;
+
+CreateNew.propTypes = {
+  addNew: PropTypes.func,
+  setNotification: PropTypes.func.isRequired,
+};
